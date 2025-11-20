1785035ad0725bf82bef35845a72a8e3e049a686
Arun <arunlokesh78@gmail.com>
2025-11-20 12:46:46 +0530

    updated-codes

--- src/Components/Certificates.jsx
+++ src/Components/Certificates.jsx
@@ -4,33 +4,27 @@ function Certificates() {
   <h2>Certificates</h2>
   <div class="certificates-container">
     <div class="certificate-card">
-      <h3>Full-Stack Web Development</h3>
       <img src="/Screenshot 2025-07-23 170248.png" alt="Udemy" loading="lazy" />
       <a class="view-button" href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-bdfb3cc2-4926-4098-879e-3a139dbcd9a5.pdf" target="_blank">View Certificate</a>
     </div>
 <div class="certificate-card">
-      <h3>Intro Front-end development</h3>
       <img src="/front.png" alt="Front-end Certificate" loading="lazy" />
       <a class="view-button" href="https://coursera.org/share/d6872cd5b154af1edbdf13ea72f513a3" target="_blank">View Certificate</a>
     </div>
 
     <div class="certificate-card">
-      <h3>HTML </h3>
       <img src="/html.png" alt="HTML Certificate" loading="lazy" />
       <a class="view-button" href="https://storage.googleapis.com/programminghub/certificate%2F1753258489620.pdf" target="_blank">View Certificate</a>
     </div>
     <div class="certificate-card">
-      <h3>CSS </h3>
       <img src="/CSS.png" alt="CSS Certificate" loading="lazy" />
       <a class="view-button" href="https://storage.googleapis.com/programminghub/certificate%2F1762783034331.pdf" target="_blank">View Certificate</a>
     </div>
     <div class="certificate-card">
-      <h3>JavaScript</h3>
       <img src="/JavaScript.png" alt="JS Certificate" loading="lazy" />
       <a class="view-button" href="https://storage.googleapis.com/programminghub/certificate%2F1746940330471.pdf" target="_blank">View Certificate</a>
     </div>
     <div class="certificate-card">
-      <h3>Node.JS </h3>
       <img src="/Node.Js.png" alt="NodeJS Certificate" loading="lazy" />
       <a class="view-button" href="https://storage.googleapis.com/programminghub/certificate%2F1746945869881.pdf" target="_blank">View Certificate</a>
     </div>
