50ad47cadd4bcea901d15e90d3a3cff6f8dd6adf
Arun <arunlokesh78@gmail.com>
2025-11-27 17:14:49 +0530

    updated-visual-changes

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -11,6 +11,7 @@ function Projects() {
 The app uses Puter.js for generating ATS insights, scoring resumes, and providing recommendations. 
 Features template-based for AI,Puter.js , clean UI components, and smooth navigation for checking ATS 
 compatibility in real-time.</p>
+<p id="mobile">AI resume analyzer using React, TypeScript, Tailwind, and Puter.js to score resumes and generate ATS insights. Features template-based analysis and a clean, responsive UI.</p>
      <div class="project-buttons">
      <a href="#" target="_blank" class="site-link">View Site</a>
      <a href="https://github.com/ArunKumarWebDeveloper/AI-Resume-Analayser" target="_blank" class="github-link">
@@ -26,6 +27,7 @@ compatibility in real-time.</p>
 Integrated the Spoonacular API to fetch recipes, ingredients, and step-by-step instructions. 
 Implements dynamic search, API data rendering, and clean component structure for a smooth user 
 experience.</p>
+<p id="mobile">Recipe search app built with React and CSS, featuring a clean, responsive UI. Integrates the Spoonacular API to fetch recipes, ingredients, and instructions with dynamic search.</p>
       <div class="project-buttons">
      <a href="#" target="_blank" class="site-link">View Site</a>
      <a href="https://github.com/ArunKumarWebDeveloper/FlavorFind" target="_blank" class="github-link">
@@ -41,6 +43,7 @@ experience.</p>
 Uses Google Books API for fetching book data and features secure authentication using bcrypt hashing. 
 Designed with a clean, modern UI and responsive layout while handling API requests, user login, and 
 database operations.</p>
+<p id="mobile">Recipe search app built with React and CSS, featuring a clean, responsive UI. Integrates the Spoonacular API to fetch recipes, ingredients, and instructions with dynamic search.</p>
        <div class="project-buttons">
       <a href="#" target="_blank" class="site-link">View Site</a>
       <a href="https://github.com/ArunKumarWebDeveloper/Book-Library" target="_blank" class="github-link">
